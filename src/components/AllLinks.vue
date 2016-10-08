293c7ee27bb558212926307d2b587872bd1ad773
Pattelicious <patrick.voss1808@gmail.com>
2016-10-08 21:32:54 +0200

    Refactore v-for entry to component

--- src/components/AllLinks.vue
+++ src/components/AllLinks.vue
@@ -1,40 +1,17 @@
 <template>
   <div class="columns is-multiline is-mobile">
     <div class="column is-4 is-one-quarter" v-for="(link, index) in links">
-      <div class="box">
-        <article class="media">
-          <div class="media-content">
-            <div class="content">
-              <p>
-                <strong>{{link.title}}</strong>
-                <br>
-                {{link.notes}}
-              </p>
-            </div>
-            <nav class="level">
-              <div class="level-left">
-                <a class="level-item">
-                  <span class="icon is-small"><i class="fa fa-reply"></i></span>
-                </a>
-                <a class="level-item">
-                  <span class="icon is-small"><i class="fa fa-retweet"></i></span>
-                </a>
-                <a class="level-item">
-                  <span class="icon is-small"><i class="fa fa-heart"></i></span>
-                </a>
-              </div>
-            </nav>
-          </div>
-        </article>
-      </div>
+      <link-item :link="link" />
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  methods: {
+import Link from './Link'
 
+export default {
+  components: {
+    'link-item': Link
   },
   computed: {
     links () {
---
+++ src/components/Link.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="box">
+    <article class="media">
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong>{{link.title}}</strong>
+            <br>
+            <span v-html="compiledMarkdown"></span>
+          </p>
+        </div>
+        <nav class="level">
+          <div class="level-left">
+            <a class="level-item">
+              <span class="icon is-small"><i class="fa fa-reply"></i></span>
+            </a>
+            <a class="level-item">
+              <span class="icon is-small"><i class="fa fa-retweet"></i></span>
+            </a>
+            <a class="level-item">
+              <span class="icon is-small"><i class="fa fa-heart"></i></span>
+            </a>
+          </div>
+        </nav>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script>
+import marked from 'marked'
+
+export default {
+  props: ['link'],
+  methods: {
+
+  },
+  computed: {
+    links () {
+      return this.$store.getters.links
+    },
+    compiledMarkdown () {
+      return marked(this.$options.propsData.link.notes, {sanitize: true})
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
