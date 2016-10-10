53b22705138c7e5550bfa8c56132886052c105c8
Pattelicious <patrick.voss1808@gmail.com>
2016-10-10 20:06:50 +0200

    Add request header for authentaction

--- src/services/linkService.js
+++ src/services/linkService.js
@@ -2,25 +2,26 @@ import Vue from 'vue'
 import authService from './authService'
 
 const BASE_URL = 'http://localhost:3000/links'
+const headers = { headers: authService.getAuthHeader() }
 
 export default {
   fetchAll () {
-    return Vue.http.get(BASE_URL, { headers: authService.getAuthHeader() })
+    return Vue.http.get(BASE_URL, headers)
       .then((response) => Promise.resolve(response.data))
       .catch((error) => Promise.reject(error))
   },
   postLink (request) {
-    return Vue.http.post(BASE_URL, request)
+    return Vue.http.post(BASE_URL, request, headers)
       .then((response) => Promise.resolve(response.data))
       .catch((error) => Promise.reject(error))
   },
   deleteLink (request) {
-    return Vue.http.delete(BASE_URL + '/' + request, request)
+    return Vue.http.delete(BASE_URL + '/' + request, request, headers)
       .then((response) => Promise.resolve(response.data))
       .catch((error) => Promise.reject(error))
   },
   updateLink (request) {
-    return Vue.http.put(BASE_URL + '/' + request.id, request)
+    return Vue.http.put(BASE_URL + '/' + request.id, request, headers)
       .then((response) => Promise.resolve(response.data))
       .catch((error) => Promise.reject(error))
   }
